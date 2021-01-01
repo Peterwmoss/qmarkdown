b086aadea88412d33f66dfd45c02ec74f3735a7c
Peter Moss <peter@moss.dk>
2021-01-01 14:17:16 +0100

    Fixed filesystem library in resgen

--- src/resgen.cpp
+++ src/resgen.cpp
@@ -3,8 +3,10 @@
 
 #if __has_include(<filesystem>)
 #include <filesystem>
+#define FILESYSTEM filesystem
 #elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
+#define FILESYSTEM std::experimental::filesystem
 #endif
 
 #include <QString>
